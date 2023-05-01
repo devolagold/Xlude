615402af5830c13f776d2f00b84e80a4abedc8f4
dev_olagold <olagoldlanre@gmail.com>
2023-05-01 14:58:50 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     <>
       <Navbar/>
       <Hero/>
-      {/* <Trusted/> */}
+      <Trusted/>
       {/* <Organized/> */}
       {/* <Experience/> */}
       {/* <Simple/> */}
