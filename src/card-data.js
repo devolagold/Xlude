34c6645dca289cd58274907fb6f0fa95f3d64c87
dev_olagold <olagoldlanre@gmail.com>
2023-05-02 18:52:11 +0100

    changes

--- src/card-data.js
+++ src/card-data.js
@@ -1,19 +1,19 @@
 export default [
     {
         id: 1,
-        icon: '/src/assets/seamless-transaction.svg',
+        icon: '/src/assets/seamless-transaction.png',
         title: 'Seamless Transaction',
         paragraph: 'Xperience the convenience of seamless transactions with a smooth and hassle-free payment process every time.'
     },
     {
         id: 2,
-        icon: '/src/assets/zero-charges.svg',
+        icon: '/src/assets/zero-charges.png',
         title: 'Zero Charges',
         paragraph: 'Enjoy the freedom of zero charges with our platform, providing a cost-effective solution to managing your bills without any hidden fees.'
     },
     {
         id: 3,
-        icon: '/src/assets/safe-secure.svg',
+        icon: '/src/assets/safe-secure.png',
         title: 'Safe & Secure',
         paragraph: 'Rest easy with our safe and secure platform, offering state-of-the-art security measures to protect your sensitive financial information.'
     }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card(props) {
     return (
         <div className="card flex flex-col space-y-16 px-5 py-8 bg-grey-2 hover:bg-black hover:text-white md:w-1/3 rounded-[10px]">
             <div className="icon bg-primary w-fit p-3 rounded-full md:w-1/4 xl:w-max">
-                <img src={props.icon} className=''/>
+                <img src={props.icon} className='' width="30px" />
             </div>
             <div className="details">
                 <h1 className='mt-7 mb-3 text-[24px] text-primary font-semibold md:mb-1 md:mt-0  lg:mb-3 md:text-[18px] lg:text-[20px] xl:text-[24px]'>
